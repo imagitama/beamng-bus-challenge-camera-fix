deb2440763105b554c9f280b16df5bfbae598e7f
Jared Williams <accounts@jaredwilliams.com.au>
2026-09-24 09:50:28 +0800

    Package script

--- package.ps1
+++ package.ps1
@@ -5,8 +5,14 @@ $zipPath = "$dirPath/BusChallengeCameraFix-$VERSION.zip"
 
 Write-Host "Packaging..."
 
+Remove-Item -Path $dirPath -Recurse -Force
+
 mkdir $dirPath
 
-Compress-Archive -Path ./mod -DestinationPath $zipPath -Force
+
+Compress-Archive -Path ./mod -DestinationPath $zipPath -Update
+
+$files = @("README.md", "LICENSE", "VERSION.txt")
+Compress-Archive -Path $files -DestinationPath $zipPath -Update
 
 Write-Host "Packaged to $zipPath"
